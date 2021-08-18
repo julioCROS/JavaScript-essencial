36b60534901b4f1e2155cab92422de186ece4cc5
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-08-18 20:30:45 -0300

    [+] Adição de Classes

---
+++ Classes/index.js
@@ -0,0 +1,23 @@
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  }
+}
+
+function Pessoa2(nome,sobrenome){
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.falar = function () {
+  console.log(`${this.nome} está falando.`);
+};
+
+const p1 = new Pessoa('Julio', 'Cesar');
+const p2 = new Pessoa2('Kim', 'Jiwoo');
+console.log(p2.falar());
